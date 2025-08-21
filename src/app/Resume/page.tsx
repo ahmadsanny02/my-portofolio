71bf25199da800c98366d9c93394e732c92d8c2e
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-08-21 18:51:28 +0700

    Delete page Resume, and add button Download CV to header menu or navigation

--- src/app/Resume/page.tsx
+++
@@ -1,9 +0,0 @@
-import Resume from "../components/Resume";
-
-export default function ResumePage() {
-    return (
-        <div className="">
-            <Resume />
-        </div>
-    )
-}
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
 
     const navLinks = [
         { name: "Home", href: "/", icon: <House /> },
-        { name: "Resume", href: "/Resume", icon: <FileText /> },
         { name: "Portfolio", href: "/Portfolio", icon: <AppWindow /> },
+
     ]
 
     return (
@@ -18,7 +18,7 @@ export default function Header() {
                     <h1 className="text-2xl hidden lg:block font-semibold ps-5">
                         {pathname == "/skills" ? "My Skills" : pathname == "/Portfolio" ? "Portfolio" : pathname == "/certificates" ? "Certificates" : "My Portfolio"}
                     </h1>
-                    <ul className="flex gap-5 bg-white/10 p-5 rounded-bl-2xl rounded-tr-2xl max-lg:rounded-2xl">
+                    <ul className="flex gap-5 bg-white/10 p-5 items-center rounded-bl-2xl rounded-tr-2xl max-lg:rounded-2xl">
                         {navLinks.map((link, index) => (
                             <li className="" key={index}>
                                 <Link href={link.href} className={`hover:text-blue-500 transition-all duration-300 ease-in-out ${pathname == link.href ? "text-blue-500 text-shadow-white" : ""}`}>
@@ -29,6 +29,10 @@ export default function Header() {
                                 </Link>
                             </li>
                         ))}
+                        <li className="flex gap-2 items-center">
+                            <FileText />
+                            <a href="/assets/cv/CV_Ahmad_Sani_Jabarulloh_Frontend_Developer.pdf" download className="">Download CV</a>
+                        </li>
 
                     </ul>
                 </nav>
